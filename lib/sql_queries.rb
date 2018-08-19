6479420da17ea853575824cb075b611401c49432
Sushi Chanrai <sushi@pathworks.co>
2018-08-19 14:23:46 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,17 +15,17 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT name, MIN(age) FROM bears;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT color, COUNT(color) FROM bears GROUP BY color HAVING COUNT(color) > 2;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * FROM bears WHERE temperament = 'aggressive';"
 end
