de8cfb00a9c08e3a8ecc3174b192402f7c4fe635
Sushi Chanrai <sushi@pathworks.co>
2018-08-19 12:34:26 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, alive) VALUES ('Mr. Chocolate', 2, 'F', 1);
+INSERT INTO bears (name, age, gender, alive) VALUES ('Rowdy', 3, 'M', 1);
+INSERT INTO bears (name, age, gender, alive) VALUES ('Tabitha', 5, 'F', 0);
+INSERT INTO bears (name, age, gender, alive) VALUES ('Sergeant Brown', 1, 'F', 1);
+INSERT INTO bears (name, age, gender, alive) VALUES ('Melissa', 8, 'F', 0);
+INSERT INTO bears (name, age, gender, alive) VALUES ('Grinch', 2, 'M', 1);
+INSERT INTO bears (name, age, gender, alive) VALUES ('Wendy', 5, 'F', 1);
+INSERT INTO bears (name, age, gender, alive) VALUES (NULL, NULL, 'M', 1);
